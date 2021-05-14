8205997cb5fce4e55389eb10d04285206ca87442
Christina24 <christinagabler@hotnail.de>
2021-05-14 15:52:57 +0200

    Aufgabe_8_change

--- Aufgabe_8/DrumPad2.js
+++ Aufgabe_8/DrumPad2.js
@@ -1,67 +1,74 @@
-window.addEventListener("load", function () {
-    document.querySelector("#Sound1").addEventListener("mousedown", function () { playSample("kick.mp3"); });
-    document.querySelector("#Sound2").addEventListener("mousedown", function () { playSample("hihat.mp3"); });
-    document.querySelector("#Sound3").addEventListener("mousedown", function () { playSample("snare.mp3"); });
-    document.querySelector("#Sound4").addEventListener("mousedown", function () { playSample("A.mp3"); });
-    document.querySelector("#Sound5").addEventListener("mousedown", function () { playSample("C.mp3"); });
-    document.querySelector("#Sound6").addEventListener("mousedown", function () { playSample("F.mp3"); });
-    document.querySelector("#Sound7").addEventListener("mousedown", function () { playSample("G.mp3"); });
-    document.querySelector("#Sound8").addEventListener("mousedown", function () { playSample("laugh-1.mp3"); });
-    document.querySelector("#Sound9").addEventListener("mousedown", function () { playSample("laugh-2.mp3"); });
-    document.querySelector("#Play").addEventListener("click", PlayTheBeat);
-    document.querySelector("#Record").addEventListener("click", RecordTheBeat);
-    document.querySelector("#Delete").addEventListener("click", DeleteTheBeat);
-    document.querySelector("#Remix").addEventListener("click", RemixBeat);
-});
-var sequence = ["kick.mp3", "hihat.mp3", "snare.mp3", "hihat.mp3"];
-var kick;
-var record = false;
-function playSample(mp3) {
-    var sound = new Audio(mp3);
-    sound.play();
-    if (record == true) {
-        sequence.push(mp3);
+var aufgabe8;
+(function (aufgabe8) {
+    window.addEventListener("load", function () {
+        document.querySelector("#Sound1").addEventListener("mousedown", function () { playSample("kick.mp3"); });
+        document.querySelector("#Sound2").addEventListener("mousedown", function () { playSample("hihat.mp3"); });
+        document.querySelector("#Sound3").addEventListener("mousedown", function () { playSample("snare.mp3"); });
+        document.querySelector("#Sound4").addEventListener("mousedown", function () { playSample("A.mp3"); });
+        document.querySelector("#Sound5").addEventListener("mousedown", function () { playSample("C.mp3"); });
+        document.querySelector("#Sound6").addEventListener("mousedown", function () { playSample("F.mp3"); });
+        document.querySelector("#Sound7").addEventListener("mousedown", function () { playSample("G.mp3"); });
+        document.querySelector("#Sound8").addEventListener("mousedown", function () { playSample("laugh-1.mp3"); });
+        document.querySelector("#Sound9").addEventListener("mousedown", function () { playSample("laugh-2.mp3"); });
+        document.querySelector("#Play").addEventListener("click", PlayTheBeat);
+        document.querySelector("#Record").addEventListener("click", RecordTheBeat);
+        document.querySelector("#Delete").addEventListener("click", DeleteTheBeat);
+        document.querySelector("#Remix").addEventListener("click", RemixBeat);
+    });
+    var mp3 = ["kick.mp3", "hihat.mp3", "snare.mp3", "A.mp3", "C.mp3", "F.mp3", "G.mp3", "laugh-1.mp3", "laugh-2.mp3"];
+    var sequence = [];
+    sequence[0] = "kick.mp3";
+    sequence[1] = "hihat.mp3";
+    sequence[2] = "snare.mp3";
+    sequence[3] = "hihat.mp3";
+    var kick;
+    var record = false;
+    function playSample(mp3) {
+        var sound = new Audio(mp3);
+        sound.play();
+        if (record == true) {
+            sequence.push(mp3);
+        }
     }
-}
-function PlayTheBeat() {
-    var index = 0;
-    if (document.getElementById("Play").classList.contains("fa-play")) {
-        document.getElementById("Play").classList.remove("fa-play");
-        document.getElementById("Play").classList.add("fa-pause");
-        kick = setInterval(mytone, 300);
-        record = false;
-        console.log("Play");
+    function PlayTheBeat() {
+        var index = 0;
+        if (document.getElementById("Play").classList.contains("fa-play")) {
+            document.getElementById("Play").classList.remove("fa-play");
+            document.getElementById("Play").classList.add("fa-pause");
+            kick = setInterval(mytone, 300);
+            record = false;
+            console.log("Play");
+        }
+        else {
+            document.getElementById("Play").classList.remove("fa-pause");
+            document.getElementById("Play").classList.add("fa-play");
+            clearInterval(kick);
+            console.log("Pause");
+        }
+        function mytone() {
+            playSample(sequence[index]);
+            index += 1;
+            if (index > (sequence.length - 1))
+                index = 0;
+            console.log(sequence[index]);
+        }
     }
-    else {
-        document.getElementById("Play").classList.remove("fa-pause");
-        document.getElementById("Play").classList.add("fa-play");
-        clearInterval(kick);
-        console.log("Pause");
+    function DeleteTheBeat() {
+        sequence.length = 0;
+        console.log("Deleting beat");
     }
-    function mytone() {
-        playSample(sequence[index]);
-        index += 1;
-        if (index > (sequence.length - 1))
-            index = 0;
-        console.log(sequence[index]);
+    function RecordTheBeat() {
+        record = true;
+        console.log("Recording beat");
     }
-}
-function DeleteTheBeat() {
-    sequence.length = 0;
-    console.log("Deleting beat");
-}
-function RecordTheBeat() {
-    record = true;
-    console.log("Recording beat");
-}
-var mp3 = ["kick.mp3", "hihat.mp3", "snare.mp3", "A.mp3", "C.mp3", "F.mp3", "G.mp3", "laugh-1.mp3", "laugh-2.mp3"];
-var i = 0;
-function RemixBeat() {
-    sequence.length = 0;
-    for (i = 0; i < 9; i++) {
-        var i_1 = Math.floor(Math.random() * 9);
-        mp3.push(i_1);
-        console.log("Remix beat");
+    function RemixBeat() {
+        sequence.length = 0;
+        for (var i = 0; i < 3; i++) {
+            var index = Math.floor(Math.random() * 8);
+            console.log(index);
+            playSample(mp3[index]);
+            console.log("Remix beat");
+        }
     }
-}
+})(aufgabe8 || (aufgabe8 = {}));
 //# sourceMappingURL=DrumPad2.js.map
--- Aufgabe_8/DrumPad2.ts
+++ Aufgabe_8/DrumPad2.ts
@@ -1,72 +1,81 @@
-window.addEventListener("load", function (): void {
+namespace aufgabe8 {
 
-    document.querySelector("#Sound1").addEventListener("mousedown", function(): void {playSample("kick.mp3"); });
-    document.querySelector("#Sound2").addEventListener("mousedown", function(): void {playSample("hihat.mp3"); });
-    document.querySelector("#Sound3").addEventListener("mousedown", function(): void {playSample("snare.mp3"); });
-    document.querySelector("#Sound4").addEventListener("mousedown", function(): void {playSample("A.mp3"); });
-    document.querySelector("#Sound5").addEventListener("mousedown", function(): void {playSample("C.mp3"); });
-    document.querySelector("#Sound6").addEventListener("mousedown", function(): void {playSample("F.mp3"); });
-    document.querySelector("#Sound7").addEventListener("mousedown", function(): void {playSample("G.mp3"); });
-    document.querySelector("#Sound8").addEventListener("mousedown", function(): void {playSample("laugh-1.mp3"); });
-    document.querySelector("#Sound9").addEventListener("mousedown", function(): void {playSample("laugh-2.mp3"); });
-    document.querySelector("#Play").addEventListener("click", PlayTheBeat);
-    document.querySelector("#Record").addEventListener("click", RecordTheBeat);
-    document.querySelector("#Delete").addEventListener("click", DeleteTheBeat);
-    document.querySelector("#Remix").addEventListener("click", RemixBeat); 
-});
+    window.addEventListener("load", function (): void {
+
+        document.querySelector("#Sound1").addEventListener("mousedown", function (): void { playSample("kick.mp3"); });
+        document.querySelector("#Sound2").addEventListener("mousedown", function (): void { playSample("hihat.mp3"); });
+        document.querySelector("#Sound3").addEventListener("mousedown", function (): void { playSample("snare.mp3"); });
+        document.querySelector("#Sound4").addEventListener("mousedown", function (): void { playSample("A.mp3"); });
+        document.querySelector("#Sound5").addEventListener("mousedown", function (): void { playSample("C.mp3"); });
+        document.querySelector("#Sound6").addEventListener("mousedown", function (): void { playSample("F.mp3"); });
+        document.querySelector("#Sound7").addEventListener("mousedown", function (): void { playSample("G.mp3"); });
+        document.querySelector("#Sound8").addEventListener("mousedown", function (): void { playSample("laugh-1.mp3"); });
+        document.querySelector("#Sound9").addEventListener("mousedown", function (): void { playSample("laugh-2.mp3"); });
+        document.querySelector("#Play").addEventListener("click", PlayTheBeat);
+        document.querySelector("#Record").addEventListener("click", RecordTheBeat);
+        document.querySelector("#Delete").addEventListener("click", DeleteTheBeat);
+        document.querySelector("#Remix").addEventListener("click", RemixBeat);
+    });
+
+    var mp3: string[] = ["kick.mp3", "hihat.mp3", "snare.mp3", "A.mp3", "C.mp3", "F.mp3", "G.mp3", "laugh-1.mp3", "laugh-2.mp3"];
+    var sequence = [];
+    sequence[0] = "kick.mp3";
+    sequence[1] = "hihat.mp3";
+    sequence[2] = "snare.mp3";
+    sequence[3] = "hihat.mp3";
+    var kick: number;
+    var record: boolean = false;
 
 
-var sequence: string [] = ["kick.mp3", "hihat.mp3", "snare.mp3", "hihat.mp3"];
-var kick: number;
-var record: boolean = false;
-
-
-function playSample(mp3: string): void  {
+    function playSample(mp3: string): void {
         var sound: HTMLAudioElement = new Audio(mp3);
         sound.play();
         if (record == true) {
             sequence.push(mp3);
-        }}
+        }
+    }
 
-function PlayTheBeat(): void {
+    function PlayTheBeat(): void {
         var index: number = 0;
         if (document.getElementById("Play").classList.contains("fa-play")) {
             document.getElementById("Play").classList.remove("fa-play");
             document.getElementById("Play").classList.add("fa-pause");
             kick = setInterval(mytone, 300);
             record = false;
-            console.log("Play"); }
-          else {
+            console.log("Play");
+        }
+        else {
             document.getElementById("Play").classList.remove("fa-pause");
             document.getElementById("Play").classList.add("fa-play");
             clearInterval(kick);
             console.log("Pause");
         }
 
-        function mytone (): void {
-            playSample(sequence [index]);
+        function mytone(): void {
+            playSample(sequence[index]);
             index += 1;
             if (index > (sequence.length - 1)) index = 0;
-            console.log(sequence [index] );
-        }}
-        
-function DeleteTheBeat (): void {
-        sequence.length = 0;
-        console.log("Deleting beat"); }
-    
-function RecordTheBeat (): void {
-        record = true;
-        console.log("Recording beat"); } 
-
-var mp3: string [] = ["kick.mp3", "hihat.mp3", "snare.mp3", "A.mp3", "C.mp3", "F.mp3", "G.mp3", "laugh-1.mp3", "laugh-2.mp3"];
-var i: number = 0;
-
-function RemixBeat (): void {
-    sequence.length = 0;
-    for (i = 0; i < 9; i++) {
-    const i: number = Math.floor(Math.random() * 9);
-    mp3.push(i);
-    console.log("Remix beat");
+            console.log(sequence[index]);
         }
     }
-    
+
+    function DeleteTheBeat(): void {
+        sequence.length = 0;
+        console.log("Deleting beat");
+    }
+
+    function RecordTheBeat(): void {
+        record = true;
+        console.log("Recording beat");
+    }
+
+    function RemixBeat(): void {
+        sequence.length = 0;
+        for (var i: number = 0; i < 3; i++) {
+            const index: number = Math.floor(Math.random() * 8);
+            console.log(index);
+            playSample (mp3 [index] ); 
+            console.log("Remix beat");
+        }
+    }
+    }
